41f2d24d683b43c95fc6c6ed4298e5dc05170fb4
Aarsh Patel <aarsh937@gmail.com>
2021-11-21 23:05:41 -0500

    live api

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const API = "https://pioneer-mt65dayzsa-uc.a.run.app/";
-const API = "http://localhost:5000/";
+const API = "https://pioneer-mt65dayzsa-uc.a.run.app/";
+// const API = "http://localhost:5000/";
 
 const tradeHistoryAll = () => {
   return axios.get(`${API}trades/history/all`);
